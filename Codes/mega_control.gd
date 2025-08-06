9fd03e202348a099e0e3cff56fbaa8c15b6159e6
Juan Hernandez <juanpablohernandeza2@gmail.com>
2025-08-06 15:23:52 -0700

    2nd continuation

--- Codes/mega_control.gd
+++ Codes/mega_control.gd
@@ -1,25 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 10.0
-@export var jump_power = 10.0
+var velocidad = 200
+var brinco = -400
+var gravedad = 1000
 
-var speed_multiplier = 30.0
-var jump_multiplier = 30.0
-var direction = 0
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	add_to_group("jugador")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y = gravity * delta
+	var direccion = Input.get_axis("ui_left","ui_right")
+	velocity.x = direccion * velocidad
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y  = jump_power * jump_multiplier
-		
-	direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed * speed_multiplier
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
-		
+	if not is_on_floor(): 
+		velocity.y += gravedad * delta
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor(): 
+		velocity.y = brinco
+	
 	move_and_slide()
