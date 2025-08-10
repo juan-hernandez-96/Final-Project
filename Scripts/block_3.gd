d431f9140c27c45a81370a9b420d4e98b3ec062c
Juan Hernandez <juanpablohernandeza2@gmail.com>
2025-08-09 20:32:53 -0700

    R01

---
+++ Scripts/block_3.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+enum TipoPlataforma {REBOTE}
+@export var type: TipoPlataforma = TipoPlataforma.REBOTE;
+@export var fuerza_rebote := 2.0 
+
+func _ready():
+	monitorable = true
+	monitoring = true
+	actualizar_plataforma()
+
+func actualizar_plataforma():
+	match type:
+		TipoPlataforma.REBOTE:
+			$Sprite2D.modulate = Color. DIM_GRAY
+
+func _on_Area2D_body_entered(body: Node2D) -> void:
+	if body.is_in_group("character"):
+		match type:
+			TipoPlataforma.REBOTE:
+				if body.has_method("puede_rebotar"):
+					body.puede_rebotar(fuerza_rebote)
+				else: 
+					body.velocity.y = body.brinco * fuerza_rebote 
